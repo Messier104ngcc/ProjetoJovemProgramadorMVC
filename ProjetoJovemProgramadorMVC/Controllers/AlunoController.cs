5e2fb8f2b0868cb336e1a1e3117bc2056124ff2c
Messier104ngcc <messier104ngc4594@gmail.com>
2024-08-18 11:43:31 -0300

    Alteracao 06/08 adicionando controler e vies.

---
+++ ProjetoJovemProgramadorMVC/Controllers/AlunoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProjetoJovemProgramadorMVC.Controllers
+{
+    public class AlunoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
