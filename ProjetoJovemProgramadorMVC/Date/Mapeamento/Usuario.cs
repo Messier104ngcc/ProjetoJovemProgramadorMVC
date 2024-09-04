6f39e39d8cf378dddd8037e9f8792dbb0e8d7671
Messier104ngcc <messier104ngc4594@gmail.com>
2024-09-04 19:52:06 -0300

    Revert "Inserindo dados atraves da tela de formulario."
    This reverts commit c628bff310a4acb3a40448c29f37b23b37e3654c.

--- ProjetoJovemProgramadorMVC/Date/Mapeamento/Usuario.cs
+++ ProjetoJovemProgramadorMVC/Date/Mapeamento/Usuario.cs
@@ -1,10 +1,10 @@
-﻿//namespace ProjetoJovemProgramadorMVC.Date.Mapeamento
-//{
-//    public class Usuario
-//    {
-//        public int Id { get; set; }
-//        public string Nome { get; set; }
-//        public string Username { get; set; }
-//        public string Senha { get; set; }
-//    }
-//}
+﻿namespace ProjetoJovemProgramadorMVC.Date.Mapeamento
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Username { get; set; }
+        public string Senha { get; set; }
+    }
+}
--- ProjetoJovemProgramadorMVC/Date/Repositorio/Interfaceer/IAlunoRepositorio.cs
+++ ProjetoJovemProgramadorMVC/Date/Repositorio/Interfaceer/IAlunoRepositorio.cs
@@ -5,9 +5,5 @@ namespace ProjetoJovemProgramadorMVC.Date.Repositorio.Interfaceer
     public interface IAlunoRepositorio
     {
         List<Aluno> BuscarAlunos();
-
-
-        // recebe todos os parametros digidados no formulario aluno.
-        void InserirAlunos(Aluno aluno);
     }
 }
